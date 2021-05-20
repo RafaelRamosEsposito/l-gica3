7079d1ca473b257c5ee38ed9ab32a11c8092629f
RafaelRamosEsposito <rafaelramosesposito@gmail.com>
2021-05-20 10:04:55 -0300

    Finalizei o terceiro desafio sobre lógica

---
+++ script.js
@@ -0,0 +1,13 @@
+let n = 10
+let n2 = 0
+
+for ( let teste = 1; teste <= 10; teste ++){
+    console.log (teste)
+}
+while ( n >= 1){
+    console.log (n)
+    n --
+}while (n2<=100) {
+    console.log (n2)
+    n2 +=2
+}
